ceb3be948fdb96832822d863cf298db09a9af07b
jarneke <jarne.eldrup@outlook.com>
2025-01-02 00:29:14 +0100

    D01 part two done

--- D01/README.md
+++ D01/README.md
@@ -1,3 +1,7 @@
 # Part One
 
 Part one was easy and straight forward.
+
+# part Two
+
+Part two was also really straitforward, just slightly diffrent from part one.
--- D01/index.ts
+++ D01/index.ts
@@ -1,7 +1,7 @@
 //imports
 import * as fs from "fs";
 
-const main = async () => {
+const Part1 = async () => {
     // we have 2 lists
     const list1: number[] = [];
     const list2: number[] = [];
@@ -34,8 +34,45 @@ const main = async () => {
         const sum = distances.reduce((a, b) => a + b, 0);
 
         // now we should have the solution
-        console.log(sum);
+        console.log("Part 1: " + sum);
     })
 }
 
-main();
+const Part2 = async () => {
+    // we get the 2 lists again
+    const list1: number[] = [];
+    const list2: number[] = [];
+
+    // we fill the lists with the input we read from input.txt
+    fs.readFile("./input.txt", "utf8", (err, data) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        const lines = data.split("\n");
+        lines.forEach((line) => {
+            const [num1, num2] = line.split("   ");
+            list1.push(Number(num1));
+            list2.push(Number(num2));
+        });
+
+        // we now need to find "similarities"
+        const similarities: number[] = [];
+        list1.forEach((num1, index) => {
+            // we count the amount of times num1 appears in list2
+            let count = 0;
+            list2.forEach((num2) => { if (num1 === num2) count++; });
+            // similarity = num1 * count
+            similarities.push(num1 * count);
+        });
+
+        // we then add all of the similarities together
+        const sum = similarities.reduce((a, b) => a + b, 0);
+
+        // now we should have the solution
+        console.log("Part 2: " + sum);
+    });
+}
+
+Part1();
+Part2();
